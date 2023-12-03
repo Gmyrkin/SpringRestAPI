c6957f0ecb4dc48f03b9bba9dd4b64de9f59cfdf
Gmyrkin <gmyrkin@gmail.com>
2023-12-03 22:04:09 +0300

    new version with Controller+

---
+++ src/main/java/ru/spb/db/rest/spring/exmp315/SpringRestAPI/controllers/UserController.java
@@ -0,0 +1,27 @@
+package ru.spb.db.rest.spring.exmp315.SpringRestAPI.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.spb.db.rest.spring.exmp315.SpringRestAPI.models.User;
+import ru.spb.db.rest.spring.exmp315.SpringRestAPI.service.UserService;
+import ru.spb.db.rest.spring.exmp315.SpringRestAPI.service.UserServiceImpl;
+
+@RestController
+@RequestMapping("/api")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/sayHello")
+    public String sayHello(){
+        return "Hello USERFUCKER";
+    }
+
+}
+
---
+++ src/main/java/ru/spb/db/rest/spring/exmp315/SpringRestAPI/service/UserServiceImpl.java
@@ -0,0 +1,55 @@
+package ru.spb.db.rest.spring.exmp315.SpringRestAPI.service;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ru.spb.db.rest.spring.exmp315.SpringRestAPI.models.User;
+import ru.spb.db.rest.spring.exmp315.SpringRestAPI.repository.UserRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Transactional
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private final UserRepository userRepository;
+
+
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public User saveUser(User user){
+        return userRepository.save(user);
+    }
+
+    @Override
+    public User findUserById(Long id){
+        Optional<User> userOptional = userRepository.findById(id);
+        if (userOptional.isPresent()) {
+            return userOptional.get();
+        }
+        return null;
+    }
+
+    @Override
+    public List<User> findAllUsers(){
+        return userRepository.findAll();
+    }
+
+    @Override
+    public User updateUser(User user){
+        return userRepository.save(user);
+    }
+
+    @Override
+    public void deleteUser(Long id){
+        userRepository.deleteById(id);
+    }
+
+
+}
